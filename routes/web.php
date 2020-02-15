943e6433c58ba3e6a632eb12ab7d155b634d29ed
Wcx <1161993909@qq.com>
2020-02-15 10:20:57 +0000

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,8 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 //创建微博
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+
+//关注
+Route::get('/users/{user}/followings','UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
