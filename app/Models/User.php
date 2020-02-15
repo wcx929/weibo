5f9aea060d2db304e43c2d24ecc9b6a3d04b808a
Wcx <1161993909@qq.com>
2020-02-15 08:00:19 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -74,6 +74,42 @@ class User extends Authenticatable
     {
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
-    }  
+    }
 
+    //粉丝 关注 关系 
+    //关注表
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //粉丝表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    //关注
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    //判断当前登录的用户 A 是否关注了用户 B
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
 }
